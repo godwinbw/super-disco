9c4a53daf1fc75ae3b7a32bf6fe6ccd2896ef8ef
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-04-13 23:41:02 -0500

    added timer to update background every 30 seconds

--- Develop/script.js
+++ Develop/script.js
@@ -87,40 +87,40 @@ var createTodaysHours = function () {
 };
 
 var getPastPresentOrFutureClassName = function (currentHour, hourLabel) {
-  console.log("getPastPresentOrFutureClassName START...");
+  //console.log("getPastPresentOrFutureClassName START...");
 
   // based on the hour label, returns either
   // "past", "present", or "future" based on current time of day
-  console.log("  hour label -> " + hourLabel);
+  //console.log("  hour label -> " + hourLabel);
 
   // convert the hourLabel to a moment object, then reformat for a comparison with current hour
   var labelHour = moment(hourLabel, "h A").format("HH");
 
-  console.log("  label hour -> " + labelHour);
-  console.log("  current hour -> " + currentHour);
+  //console.log("  label hour -> " + labelHour);
+  //console.log("  current hour -> " + currentHour);
 
   if (labelHour - currentHour > 0) {
     // the label hour is in the future
-    console.log("   labelHour " + labelHour + " is in the future");
+    //console.log("   labelHour " + labelHour + " is in the future");
     return "future";
   } else if (labelHour - currentHour == 0) {
     // the label hour is in the present
-    console.log("   labelHour " + labelHour + " is in the present");
+    //console.log("   labelHour " + labelHour + " is in the present");
     return "present";
   } else {
     // the label hour is in the past
-    console.log("   labelHour " + labelHour + " is in the past");
+    //console.log("   labelHour " + labelHour + " is in the past");
     return "past";
   }
 };
 
 var updateDescriptionRowBackgroundColor = function () {
-  console.log("updateDescriptionRowBackgroundColor START...");
+  //console.log("updateDescriptionRowBackgroundColor START...");
   // update each description row background color based on past, present, or future
 
   // get current time of day in range of 0 -23
   var currentHour = moment().format("HH");
-  console.log("   currentHour -> " + currentHour);
+  //console.log("   currentHour -> " + currentHour);
 
   //loop through all the row elements in the #today-planner element
   // each one is a row.  get the attribute hour-label to see the hour of that row
@@ -129,20 +129,35 @@ var updateDescriptionRowBackgroundColor = function () {
     .each((index, element) => {
       //get the hour-label attribute
       var hourLabel = $(element).attr("hour-label");
-      console.log("hour-label -> " + hourLabel);
+      //console.log("hour-label -> " + hourLabel);
 
       // see if this is past, present, or future
       var bgClass = getPastPresentOrFutureClassName(currentHour, hourLabel);
-      console.log("bgClass -> " + bgClass);
+      //console.log("bgClass -> " + bgClass);
 
       // assign this bgClass to all descendents of this element with class = description
       $(element).children(".description").addClass(bgClass);
     });
 };
 
+var updateTick = function () {
+  // this updates the description background colors everytime it gets called
+  console.log("updateTick START!");
+  updateDescriptionRowBackgroundColor();
+};
+
 // execute when page loads
+
+// 1. first, update the banner with today's date
 updateBannerWithTodaysDate();
 
+// 2. create today's hours
 createTodaysHours();
 
+// 3. load the contents of the rows with any saved data
+
+// 4. update the description row backgorund color
 updateDescriptionRowBackgroundColor();
+
+//5. start a timer, that updates the background color every 30 seconds
+setInterval(updateTick, 1000 * 30);
